e30df490d1bc85c825ab1b6732e16cf540298d1f
Sndie9 <117831167+Sndie9@users.noreply.github.com>
2024-06-07 16:14:18 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -49,6 +49,35 @@ def Readfiche(post_id):
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
 
+# Fonction pour créer une clé "authentifie_user" dans la session utilisateur
+def est_authentifie_user():
+    return session.get('authentifie_user')
+
+@app.route('/authentification_user', methods=['GET', 'POST'])
+def authentification_user():
+    if request.method == 'POST':
+        if request.form['username'] == 'user' and request.form['password'] == '12345':
+            session['authentifie_user'] = True
+            return redirect(url_for('ReadFicheNom', nom=request.form['nom']))
+        else:
+            return render_template('formulaire_authentification_user.html', error=True)
+
+    return render_template('formulaire_authentification_user.html', error=False)
+
+@app.route('/fiche_nom/<string:nom>')
+def ReadFicheNom(nom):
+    if not est_authentifie_user():
+        return redirect(url_for('authentification_user'))
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients WHERE nom = ?', (nom.upper(),))  # Convertir en majuscule pour la recherche
+    data = cursor.fetchall()
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('read_data.html', data=data)
+
+
 @app.route('/consultation/')
 def ReadBDD():
     conn = sqlite3.connect('database.db')
